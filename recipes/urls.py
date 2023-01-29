c626488fde081eac2e75eb2c8fee59134fc6e6cb
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-01-29 20:58:48 -0300

    Removed html files for contact and about views from app recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from recipes.views import home, sobre, contato
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
 ]
