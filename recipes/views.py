7bfe9454a63ebc249c2e32a738f7c2ab432fc4cc
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-02-02 08:57:07 -0300

    I created view for see details of one recipe and i adjusted their style

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,9 @@ def home(request):
     return render(request, "recipes/pages/home.html", context={
         'name': 'Fabio Carneiro'
     })
+
+
+def recipe(request, id):
+    return render(request, "recipes/pages/recipe-view.html", context={
+        'name': 'Fabio Carneiro'
+    })
